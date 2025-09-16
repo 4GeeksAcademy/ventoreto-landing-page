540fda282eb23facb65a70f8dffcf6792ac9c79a
ventoreto <josemigueltrader2650@gmail.com>
2025-09-16 03:56:09 +0000

    landing-page

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Footer (){
+    return (
+        <div className=" container-fluid navbar navbar-expand-lg navbar-dark bg-dark  d-flex justify-content-center "style={{width : "100%"}}>
+            <span className="text-white ">Copyright@ your Website 2025</span>
+        </div>
+    )
+}
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,51 @@
-import React from "react";
+import React from "react"
+import { Navbar } from "./Navbar"
+import { JumboTron } from "./JumboTron"
+import { Card } from "./Card"
+import {Footer} from "./Footer"
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
+export function Home(){
+    const p = ` Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias quidem totam quod 
+                  cum veritatis qui eligendi vitae itaque amet.`
+              
+    return(
+        <React.Fragment>
+            <Navbar/>
+            <JumboTron/>
+            <div className ="d-flex container gap-3 pt-3 px-0">
+                <Card 
+                    image="../image/Cascada01.jpg" 
+                    description={p} 
+                    label="Rio Momtañoso"
+                    title="Paisaje de Valle"
+                />
+                <Card 
+                    image="https://previews.123rf.com/images/scimmery/scimmery1908/scimmery190800051/128699220-the-krimml-waterfalls-in-the-high-tauern-national-park-salzburg-austria.jpg"
+                    description={p}
+                    label = "Pinos Grandes"
+                    title ="Paisaje de Pino"
+                />
+                <Card 
+                    image="https://www.wideopenspaces.com/wp-content/uploads/sites/3/2025/08/Guests-Potentially-Exposed-To-Deadly-Virus-While-Staying-At-Grand-Teton-National-Park.jpg?w=1056"
+                    description={p}
+                    label = "Aguas Tranquilas"
+                    title = "Paisaje Hermoso"
+                /> 
+                <Card 
+                    image="../image/Rio01.jpg"
+                    description ={p}
+                    label ="Piedras Preciosa"
+                    title ="Paisaje de Piedras"
+                />  
+            </div>
+            <Footer/>
+        </React.Fragment>
+    )
+}
+                
+
+                
+                    
             
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
-export default Home;
+            
